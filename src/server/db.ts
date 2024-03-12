2aa780593bd06733daae9a9d6c7c7fe4eaa8c7fc
a <a@pop-os.localdomain>
2024-03-12 09:30:39 +0100

    Tried to make it run on vercel
    Prisma is causing issues: https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/vercel-caching-issue

--- src/server/db.ts
+++ src/server/db.ts
@@ -15,3 +15,8 @@ const globalForPrisma = globalThis as unknown as {
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+// COnsider setting the runtime to edge to mimic production,
+// to save time.
+// Then it would maybe be possible to import prismaclient from prisma/client/edge,
+// without having to worry about different environments
